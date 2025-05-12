e5b2ac71f3cb03599b30ded7c430587ffb7bdd63
devesh yadav <dd4168@srmist.edu.in>
2025-05-12 13:00:41 +0530

    done with conversion

---
+++ basic/03_conversionOperation.js
@@ -0,0 +1,25 @@
+// let score = "33av";
+// console.log(typeof score);
+// console.log(typeof score);
+
+// let valInNumver = Number(score);
+// console.log(valInNumver);
+
+//"33" = 33
+//"22avs" = Nan
+//true = 1
+
+let isLoggedIn = "";
+
+let boolenIsLoggedIn = Boolean(isLoggedIn);
+console.log(boolenIsLoggedIn);
+//"" = false
+
+let someNumber = 22
+
+let stringNumber =  String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
+
+
+// ******************** OPERATIONS ******************
