f5ce0e6099a43fa0573ee919ee6318f1da12cbd2
devesh yadav <dd4168@srmist.edu.in>
2025-05-12 12:40:13 +0530

    learning with Chai Aur Code

---
+++ basic/01_variables.js
@@ -0,0 +1,13 @@
+//With CHAI AUR CODE
+
+//prefer not to use "var" because of issue in block scope and functional scope
+const accId = 123;
+let accountEmail = "dewf@gmail.com";
+var accPAssword = "w41231";
+accountCity = "Jaipur";
+
+accountEmail = "sdw@gmail.com";
+accPAssword = "w13213131";
+accountCity = "2Jaipurss";
+
+console.table([accId, accPAssword, accountEmail, accountCity, accst]);
