45ac47fa9bce1f8dee09d3d499e3da5eb5a2681b
devesh yadav <dd4168@srmist.edu.in>
2025-05-12 12:51:36 +0530

    dont with variables

---
+++ basic/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict" //treat all js code as newer version 
+
+//primitive data types
+//number, bigint, string , boolean
+//null -> standalone value
+//undefined -> value declared but not defined
+//Symbol => unique
+
+//object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
