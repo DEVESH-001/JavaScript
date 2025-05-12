e86a603de242576a83f866e2e485588ab7adbdc3
devesh yadav <dd4168@srmist.edu.in>
2025-05-12 15:39:35 +0530

    added memory

---
+++ basic/04_Memory.js
@@ -0,0 +1,17 @@
+//1. Stack(primitive), 2. Heap(Non-Primitive)
+
+// let myYoutube = "devesjuadbadotcom";
+// let anothername = myYoutube;
+
+// anothername = "ChaiOURCode";
+
+// console.log(myYoutube);
+// console.log(anothername);
+
+let user1 = { email: "Devesh@gmail.com", upi: "Sher@ybl" };
+let user2 = user1;
+
+user2.email=("yadav@hikeh.dcom");
+
+console.log(user1.email);
+console.log(user2.email);
